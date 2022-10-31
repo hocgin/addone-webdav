51db90d20650f955cf435cc1087e35847ec5aef3
hocgin <hocgin@gmail.com>
2022-11-01 01:39:25 +0800

    [新增功能](main): Update

--- src/components/FileContent/UploadFile/index.tsx
+++ src/components/FileContent/UploadFile/index.tsx
@@ -1,27 +1,42 @@
 import * as React from 'react';
-import {Dropdown, Menu} from 'antd';
-import {EventEmitter} from 'ahooks/lib/useEventEmitter';
-import {WebDavEventType} from '@/_utils/types';
-import {UploadOutlined} from '@ant-design/icons';
-import UploadButton from "@/components/FileContent/UploadFile/UploadButton";
-import styles from "./index.less";
-
+import { Dropdown, Menu } from 'antd';
+import { EventEmitter } from 'ahooks/lib/useEventEmitter';
+import { WebDavEventType } from '@/_utils/types';
+import { UploadOutlined } from '@ant-design/icons';
+import UploadButton from '@/components/FileContent/UploadFile/UploadButton';
+import styles from './index.less';
 
 const Index: React.FC<{
   children?: any;
   webDav$: EventEmitter<WebDavEventType>;
-}> = ({children, webDav$}) => {
-  return <Dropdown.Button overlay={<Menu
-    items={[{
-      label: <UploadButton webDav$={webDav$}>上传文件</UploadButton>,
-      key: 'file',
-    }, {
-      label: <UploadButton webDav$={webDav$} directory={true}>上传文件夹</UploadButton>,
-      key: 'directory',
-    }]}
-  />} type="primary">
-    <UploadButton webDav$={webDav$} className={styles.uploadButton}><UploadOutlined />{children}</UploadButton>
-  </Dropdown.Button>;
+}> = ({ children, webDav$ }) => {
+  return (
+    <Dropdown.Button
+      overlay={
+        <Menu
+          items={[
+            {
+              label: <UploadButton webDav$={webDav$}>上传文件</UploadButton>,
+              key: 'file',
+            },
+            {
+              label: (
+                <UploadButton webDav$={webDav$} directory={true}>
+                  上传文件夹
+                </UploadButton>
+              ),
+              key: 'directory',
+            },
+          ]}
+        />
+      }
+      type="primary"
+    >
+      <UploadButton webDav$={webDav$} className={styles.uploadButton}>
+        <UploadOutlined /> {children}
+      </UploadButton>
+    </Dropdown.Button>
+  );
 };
 
 export default Index;
