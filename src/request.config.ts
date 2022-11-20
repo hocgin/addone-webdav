1d9f01dc1466d1605e9be45281738bd9698dd42d
hocgin <hocgin@gmail.com>
2022-11-20 18:05:43 +0800

    [新增功能](main): 调整

--- src/request.config.ts
+++ src/request.config.ts
@@ -1,6 +1,7 @@
 import {defaultRequestOptions, LangKit} from '@hocgin/hkit';
 import Config from '@/config';
 import {message} from 'antd';
+import {WebExtension} from "@hocgin/browser-addone-kit";
 
 // 网络请求配置
 defaultRequestOptions({
@@ -11,6 +12,7 @@ defaultRequestOptions({
     if (Config.isDev()) {
       headers['X-Username'] = 'hocgin';
     }
+    headers['Authorization'] = `Bearer ${await WebExtension.kit.getUserToken()}`;
     return headers;
   },
   errorHandler: (error: any) =>
