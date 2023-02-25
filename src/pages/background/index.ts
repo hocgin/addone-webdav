955917008a825ff22c8ec5bce4ad265299391c20
hocgin <hocgin@gmail.com>
2023-02-26 00:52:33 +0800

    [新增功能](main): 调整

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,8 +1,12 @@
 import {WebExtension} from '@hocgin/browser-addone-kit';
 import {ServiceWorkerOptions} from '@hocgin/browser-addone-kit/dist/esm/browser/serviceWorker';
 import '@/request.config';
+import Config from "@/config";
 
-WebExtension.kit.serviceWorker(ServiceWorkerOptions.default);
+WebExtension.kit.serviceWorker({
+  ...ServiceWorkerOptions.default,
+  projectId: Config.getProjectId(),
+});
 
 // 点击打开弹窗
 let openNewWindow = (...args: any) => {
